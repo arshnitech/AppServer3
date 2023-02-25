0f4514cdb349f9fca82938f4f8005718008b0b61
arshni <arshni.tech@gmail.com>
2023-02-25 22:01:06 +0530

    first commit

--- routes/products.js
+++ routes/products.js
@@ -7,7 +7,7 @@ var errors = [];
 router.get('/products', helpers.loginChecker, function (req, res, next) {
 
   res.render('products', {
-    title: 'Products from AppServer1'
+    title: 'Products from AppServer2'
   });
 
 });
